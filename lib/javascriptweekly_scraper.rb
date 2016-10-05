509bd187bebf7c3f478f6e73116cdf692fb0b51d
toanhtran <toanh.t.tran@gmail.com>
2016-10-05 11:25:48 +0900

    initialize jsweekly scraper

--- lib/javascriptweekly_scraper.rb
+++ lib/javascriptweekly_scraper.rb
@@ -1,2 +1,9 @@
-class Article
+class JavascriptWeeklyScraper
+
+  def initialize(issue_number)
+    @newletter = Newsletter.new
+    @newsletter.issue_number = issue_number
+    @doc = Nokogiri::HTML(open("http://javascriptweekly.com/issues/#{issue_number}"))
+  end
+
 end
