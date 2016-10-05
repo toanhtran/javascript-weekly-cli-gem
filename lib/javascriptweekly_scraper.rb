1dd51b97b2d0a242ebb5a770103b8de65aecf733
toanhtran <toanh.t.tran@gmail.com>
2016-10-05 12:42:48 +0900

    add scrape_articles method

--- lib/javascriptweekly_scraper.rb
+++ lib/javascriptweekly_scraper.rb
@@ -16,4 +16,14 @@ class JavascriptWeeklyScraper
   def scrape_details
     @newsletter.issue_date = @doc.search("table.gowide.lonmo").text.strip.gsub("Issue #{@issue_number} - ", "").strip
   end
+
+  def scrape_articles
+    @doc.search("td[align='left'] table.gowide")[2..-1].each do |article_table|
+      a = Article.new
+      a.author = article_table.search("div:first").text.strip
+      a.title = article_table.search("a:first").text.strip
+      a.url = article_table.search("a:first").attr("href").text.strip
+      @newsletter.add_article(a)
+    end
+  end
 end
