c6d5230a7a2d9716a14a15e38ad641a27d27179b
toanhtran <toanh.t.tran@gmail.com>
2016-10-05 12:01:25 +0900

    add scrape_details method

--- lib/javascriptweekly_scraper.rb
+++ lib/javascriptweekly_scraper.rb
@@ -1,9 +1,19 @@
 class JavascriptWeeklyScraper
+  attr_accessor :newsletter
+  attr_reader :doc
 
   def initialize(issue_number)
-    @newletter = Newsletter.new
+    @newsletter = Newsletter.new
     @newsletter.issue_number = issue_number
     @doc = Nokogiri::HTML(open("http://javascriptweekly.com/issues/#{issue_number}"))
   end
 
+  def scrape
+
+    @newsletter
+  end
+
+  def scrape_details
+    @newsletter.issue_date = @doc.search("table.gowide.lonmo").text.strip.gsub("Issue #{@issue_number} - ", "").strip
+  end
 end
